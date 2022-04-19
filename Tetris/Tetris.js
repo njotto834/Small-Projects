38f627f4d32cdd179c99eeae8040edee381fee44
Nick Otto <nick83487@gmail.com>
2022-04-19 13:31:22 -0500

    Added the rest of the Tetrominoes and started messing around with them.

--- Tetris/Tetris.js
+++ Tetris/Tetris.js
@@ -7,11 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //The Tetrominoes
     const lTetromino = [
-        [1, width+1, width*2+1, 2] //Picks grid indices 1, 11, 21, 2
-        [width, width+1, width+2, width*2+2]
-        [1, width+1, width*2+1, width*2]
-        [width, width*2, width*2+1, width*2+2]
+        [1, width+1, width*2+1, 2], //1, 11, 21, 2
+        [width, width+1, width+2, width*2+2], //10, 11, 12, 22
+        [1, width+1, width*2+1, width*2], //1, 11, 21, 20
+        [width, width*2, width*2+1, width*2+2] //10, 20, 21, 22
     ]
 
+    const zTetromino = [
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1], 
+        [0, width, width+1, width*2+1] 
+    ]
     
+    const tTetromino = [
+        [1, width, width+1, width+2], 
+        [1, width+1, width+2, width*2+1], 
+        [width, width+1, width+2, width*2+1], 
+        [1, width, width+1, width*2+1] 
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1], 
+        [0, 1, width, width+1] 
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1], 
+        [width, width+1, width+2, width+3], 
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3] 
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+    let currentPosition = 4
+    let current = theTetrominoes[0][0]
+
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    draw();
+    
+    function fun(){
+        alert("Hello");
+    }
+
 })
